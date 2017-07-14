39889cdea80fa1be4ca2dcdf90057e6683d90000
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-07-14 19:58:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ os.mkdir(pwd + "/images")
 #
 #print('looking for ' + config["AFQ"] + "/tracts/*.json")
 print config["AFQ"]
-for file in glob.glob(config["AFQ"] + "/tracts/*.json"):
+for file in glob.glob(config["AFQ"] + "/*.json"):
 #for file in glob.glob("tracts/*.json"):
     with open(file) as data_file:    
         tract = json.load(data_file)
