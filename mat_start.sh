8c93e74cd7cb71586229ef4ad815d65b441c47ba
kitchell <lindseykitchell21@gmail.com>
2017-07-14 19:32:42 -0400

    update for python

---
+++ mat_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
+
+#clean up previous job (just in case)
+rm -f finished
+
+if [ $ENV == "IUHPC" ]; then
+	jobid=`qsub $SERVICE_DIR/submit.pbs`
+	#jobid=`qsub -q preempt $SERVICE_DIR/submit.pbs`
+	echo $jobid > jobid
+fi
+
+if [ $ENV == "VM" ]; then
+	nohup time $SERVICE_DIR/submit.pbs > stdout.log 2> stderr.log &
+	echo $! > pid
+fi
