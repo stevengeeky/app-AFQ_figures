c45303bce7510876393355efd25346e3be6e15d3
Lindsey Kitchell <kitchell@indiana.edu>
2017-07-18 14:15:26 -0400

    update to choose slices

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ view_up = [(0.05, 0.98, -0.21), (-0.02, -0.01, 1.00),
            (-0.01, 0.04, 1.00), (-0.20, 0.21, 0.96)]
 views = ['axial', 'sagittal_left', 'coronal', 'sagittal_right']
 
-slice_view = [48, 74, 85]
+# slice_view = [48, 74, 85]
+slice_view = [config['xslice'], config['yslice'], config['zslice']]
 
 img = nib.load(config['t1'])
 data = img.get_data()
@@ -43,25 +44,27 @@ affine = img.affine
 mean, std = data[data > 0].mean(), data[data > 0].std()
 value_range = (mean - 0.2 * std, mean + 2 * std)
 
+
 print config["AFQ"]
 for file in glob.glob(config["AFQ"] + "/*.json"):
-#for file in glob.glob("tracts/*.json"):
-    with open(file) as data_file:    
+# for file in glob.glob("tracts/*.json"):
+    with open(file) as data_file:
         tract = json.load(data_file)
     bundle = []
     for i in range(len(tract['coords'])):
-        templine = np.zeros([len(tract['coords'][i][0][0]),3])
-        templine[:,0] = tract['coords'][i][0][0]
-        templine[:,1] = tract['coords'][i][0][1]
-        templine[:,2] = tract['coords'][i][0][2]
+        templine = np.zeros([len(tract['coords'][i][0][0]), 3])
+        templine[:, 0] = tract['coords'][i][0][0]
+        templine[:, 1] = tract['coords'][i][0][1]
+        templine[:, 2] = tract['coords'][i][0][2]
         bundle.append(templine)
-    
+
     split_name = tract['name'].split(' ')
     imagename = '_'.join(split_name)
-    
-    for d in range(len(camera_pos)): #directions: axial, sagittal, coronal
+
+    for d in range(len(camera_pos)):  # directions: axial, sagittal, coronal
         renderer = window.Renderer()
-        stream_actor = actor.streamtube(bundle, colors=tract['color'],linewidth=1)                        
+        stream_actor = actor.streamtube(bundle, colors=tract['color'],
+                                        linewidth=1)
         renderer.set_camera(position=camera_pos[d],
                             focal_point=focal_point[d],
                             view_up=view_up[d])
@@ -69,25 +72,22 @@ for file in glob.glob(config["AFQ"] + "/*.json"):
         renderer.add(stream_actor)
         slice_actor = actor.slicer(data, affine, value_range)
         if d == 0:
-            slice_actor.display(z=slice_view[0])
+            slice_actor.display(z=int(slice_view[0]))
         elif d == 2:
-            slice_actor.display(y=slice_view[2])
-        else: 
-            slice_actor.display(x=slice_view[1])
+            slice_actor.display(y=int(slice_view[2]))
+        else:
+            slice_actor.display(x=int(slice_view[1]))
         renderer.add(slice_actor)
-#        
+
 #        show_m = window.ShowManager(renderer, size=(800, 700))
 #        show_m.initialize()
 #        show_m.render()
 #        show_m.start()
 #        renderer.camera_info() #get location of camera
-        
-        window.snapshot(renderer, fname='images/'+imagename+'_'+views[d]+'.png', 
-                        size=(800, 800), offscreen=True, order_transparent=False)
-    
-    
-        #window.show(renderer, size=(600, 600), reset_camera=False)
-    #    window.record(renderer, out_path= 'images/'+tract['name']+'.png', size=(600, 600))
-    
-   
+
+        window.snapshot(renderer,
+                        fname='images/'+imagename+'_'+views[d]+'.png',
+                        size=(800, 800), offscreen=True,
+                        order_transparent=False)
+
 vdisplay.stop()
